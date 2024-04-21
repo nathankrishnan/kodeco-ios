5495a11b937e8cf66a607bad27e5fcf2fa300ee7
Nathan Krishnan <nathan.a.krishnan@gmail.com>
2024-04-20 20:59:06 -0700

    Add Button values to Constants

--- module-2/ColorPicker/Constants.swift
+++ module-2/ColorPicker/Constants.swift
@@ -5,5 +5,9 @@ enum Constants {
     public static let minValue = 0.0
     public static let maxValue = 255.0
   }
-}
 
+  enum Button {
+    public static let cornerRadius = CGFloat(20)
+    public static let strokeLineWidth = CGFloat(2)
+  }
+}
