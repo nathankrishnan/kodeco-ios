6673eded8c48020829283f488003da9876b7bf51
Nathan Krishnan <nathan.a.krishnan@gmail.com>
2024-08-11 17:58:34 -0700

    Make background color of the TabBar white

--- module-6/Tasks/Tasks/TasksApp.swift
+++ module-6/Tasks/Tasks/TasksApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct TasksApp: App {
+  init() {
+    UITabBar.appearance().backgroundColor = UIColor.white
+  }
+  
   var body: some Scene {
     WindowGroup {
       TaskTabView()
