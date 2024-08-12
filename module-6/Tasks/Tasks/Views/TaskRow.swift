a335a700ac0140443ebc4c8985c213a543951102
Nathan Krishnan <nathan.a.krishnan@gmail.com>
2024-08-11 21:52:28 -0700

    Add animation in TaskRow to toggle the systemImage and color

--- module-6/Tasks/Tasks/Views/TaskRow.swift
+++ module-6/Tasks/Tasks/Views/TaskRow.swift
@@ -9,31 +9,47 @@ import SwiftUI
 
 struct TaskRow: View {
   @EnvironmentObject var taskItemStore: TaskItemStore
+  @State private var isAnimating = false
   let taskItem: TaskItem
-  
+
+  private var currentSystemImage: String {
+    if isAnimating {
+      return taskItem.isCompleted ? "square" : "checkmark.square"
+    } else {
+      return taskItem.isCompleted ? "checkmark.square" : "square"
+    }
+  }
+
   var body: some View {
     HStack {
       Text(taskItem.title)
         .font(.title3)
         .fontWeight(.semibold)
         .foregroundColor(Color.blue)
-      
+
       // Hide NaviationLink cheveron
       NavigationLink(destination: TaskItemDetail(taskItem: taskItem)) {
         EmptyView()
       }
       .opacity(0.0)
-      
+
       Spacer()
-      
+
       Button(action: {
-        withAnimation {
+        withAnimation(.easeInOut(duration: 0.4)) {
+          isAnimating = true
+        }
+        // Delay the taskItem status change to let the animation complete
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
           taskItemStore.toggleStatus(for: taskItem)
+          isAnimating = false
         }
       }) {
-        Image(systemName: taskItem.isCompleted ? "checkmark.square" : "square")
-          .foregroundStyle(taskItem.isCompleted ? .green : .red)
+        Image(systemName: currentSystemImage)
           .font(.system(size: 20, weight: .bold))
+          .foregroundStyle(isAnimating ? (taskItem.isCompleted ? .red : .green) : (taskItem.isCompleted ? .green : .red))
+          .scaleEffect(isAnimating ? 1.4 : 1.0)
+          .opacity(isAnimating ? 0.5 : 1.0)
       }
       .buttonStyle(BorderlessButtonStyle()) // Prevents triggering NavigationLink
     }
